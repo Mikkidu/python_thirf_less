b75aefeacf30cf6c78a13aa7a21b23f01410035a
Алёша <mikkidu1@gmail.com>
2022-02-28 16:25:26 +0300

    2-nd with *

--- 2seq.py
+++ 2seq.py
@@ -1,5 +1,10 @@
-numbers_str = input('Введите элементы списка через запятую: ')
-numbers_list = numbers_str.split(',')
-numbers_set = set(numbers_list)
-numbers_list = list(numbers_set)
-print('Результат: ', numbers_list)
+numbers_str = input('Введите цифры списка через запятую, точку с запятой или слеш: ')
+if numbers_str[1] in ',;/':
+    numbers_list = numbers_str.split(numbers_str[1])
+    numbers_set = set(numbers_list)
+    numbers_str = ', '.join(numbers_set)
+    print('Результат: ', numbers_str)
+else:
+    print('Ошибка. Неверно задан список.')
+## Не стал все возможные ошибки учитывать.
+## Программа работает корректно, если цифры вводить все через "," или все через ";" или все через "/".
